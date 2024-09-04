a909e77799e25640c5921b153605e8a94147c931
Meirna-kamal <Meirna.abdelrahman99@eng-st.cu.edu.eg>
2024-09-04 15:49:19 +0300

    Initialize Project

---
+++ wagtail_project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-u+1*u7+cq(kg+ut9e$1n@2bgg)o-3ixl34*4)37!ik1%y69ale"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
